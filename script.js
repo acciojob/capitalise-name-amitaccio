1cc408ddc0d8a60e5f0a32e9af2e679a2e8ddf6e
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-25 21:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let inputElm = document.getElementById("fname");
+inputElm.addEventListener("blur",()=>{
+	inputElm.value = inputElm.value.toUpperCase();
+})
+
+
+// blur is a special type event whenver we loss the focus on
+// input field this the function associated with this will be call
